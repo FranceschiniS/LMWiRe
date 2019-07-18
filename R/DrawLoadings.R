4b4008eab359ae15ceb684c39dfe41038304b27f
FranceschiniS <francseba@gmail.com>
2019-07-18 12:08:16 +0200

    Adding DrawLoadings and DrawScores with their documentation

---
+++ R/DrawLoadings.R
@@ -0,0 +1,278 @@
+#' @export DrawLoadings
+#' @title Loadings plots
+#'
+#' @description
+#' Draws Loadings plots for the SVDforPCA, PLSDA or OPLSDA functions.
+#'
+#' @param obj The objects resulting from a PCA, PLSDA or OPLSDA analysis.
+#' @param type.obj The type of object to be plotted.
+#' @param createWindow If \code{TRUE}, will create a new window for the plot.
+#' @param main Plot title. If \code{NULL}, default title is provided.
+#' @param axes Numerical vector indicating the PC axes that are drawn.
+#' @param loadingstype The type of Loadings plot, either a line plot (\code{'l'}), points (\code{'p'}) or segments (\code{'s'}).
+#' @param num.stacked Number of stacked plots.
+#' @param xlab Label for the x-axis.
+#' @param ylab Label for the y-axis.
+#' @param ang Angle to rotate the x axis labels for a better visualisation, either 0, 45 or 90 degrees.
+#' @param xaxis_type Specify if the xaxis is numerical or character
+#' @param nxaxis Number of thick marks on the xaxis for a character x variable
+#' @param hline Numerical scalar. If not \code{NULL}, an horizontal dashed line is drawn at the \code{hline} value
+#' @param xaxis_size Size of the x axis.
+#'
+#' @return A loading plot in the current device.
+#'
+#' @details
+#' Better results printping is obtained by saving the plot as an object
+#'
+#' @examples
+#'
+#' data('HumanSerum')
+#' res.PCA = SVDforPCA(HumanSerumSpectra)
+#'
+#' DrawLoadings(res.PCA, type.obj = 'PCA',
+#' createWindow = FALSE, main = 'PCA loadings plot for HumanSerum dataset',
+#'    axes = 1,  loadingstype='l', xlab = "L1")
+#'
+#' @importFrom grDevices dev.new
+#' @import ggplot2
+#' @import reshape2
+#' @import gridExtra
+
+
+
+DrawLoadings <- function(obj, type.obj = c("PCA", "PLSDA", "OPLSDA"),
+                         createWindow = FALSE, main = NULL,  axes = c(1, 2),
+                         loadingstype = c("l", "p", "s"), num.stacked = 4, xlab = NULL, ylab = NULL,
+                         ang = c("0", "45", "90"), xaxis_type = c("numerical", "character"),
+                         nxaxis = 10, hline = NULL, xaxis_size = 11) {
+  
+  checkArg(main, "str", can.be.null = TRUE)
+  checkArg(nxaxis, "num", can.be.null = FALSE)
+  checkArg(hline, "num", can.be.null = TRUE)
+  
+  
+  
+  
+  loadingstype <- match.arg(loadingstype)
+  
+  type.obj <- match.arg(type.obj)
+  xaxis_type <- match.arg(xaxis_type)
+  
+  ang <- match.arg(ang)
+  
+  if (ang %in% c("0", "45")) {
+    vjust <- 1
+    hjust <- 0.5
+  } else {
+    vjust <- 0.5
+    hjust <- 1
+  }
+  
+  
+  m <- dim(obj$original.dataset)[1]
+  nn <- dim(obj$original.dataset)[2]
+  
+  
+  
+  # axes
+  if (!is.vector(axes, mode = "numeric")) {
+    stop("axes is not a numeric vector")
+  }
+  
+  
+  # Eigenvalues
+  if (type.obj == "PCA") {
+    eig <- obj$eigval
+    # Variances in percentage
+    variance <- eig * 100/sum(eig)
+  }
+  
+  # loadings
+  if (type.obj == "OPLSDA") {
+    obj$loadings <- cbind(obj$Pp, obj$Portho)
+    colnames(obj$loadings) <- c("Pp", paste0("Po", 1:dim(obj$Portho)[2]))
+  }
+  class(obj$loadings) <- "numeric"
+  loadings <- obj$loadings
+  
+  if (type.obj != "OPLSDA"){
+    colnames(loadings) <- paste0("Loading", c(1:dim(loadings)[2]))
+  }
+  
+  loadings <- as.data.frame(loadings)
+  
+  plots <- list()
+  plot <- list()
+  Var <- rowname <- value <- NULL  # only for R CMD check
+  
+  ##########################################
+  
+  if (!is.null(ylab) && length(ylab) != 1) {
+    warning("Since ylab is longer than 1, the first element is selected")
+    ylab <- ylab[1]
+  }
+  
+  if (!is.null(xlab) && length(xlab) != 1) {
+    warning("Since xlab is longer than 1, the first element is selected")
+    xlab <- xlab[1]
+  }
+  
+  # loadings <- loadings[, axes]
+  
+  # labs
+  if (is.null(xlab)) {
+    xlab <- "Index"
+  }
+  
+  if (is.null(ylab)) {
+    ylab <- "Value"
+  }
+  
+  
+  n <- length(axes)
+  
+  
+  j <- 1
+  i <- 1
+  while (i <= n) {
+    
+    last <- min(i + num.stacked - 1, n)
+    
+    if(i<last){
+      load <- t(loadings[, i:last])
+    }else{
+      load <- as.matrix(t(loadings[, i]))
+      names(load) <- rownames(loadings)
+    }
+    melted <- reshape2::melt(load, varnames = c("rowname", "Var"))
+    if (n==1) {
+      melted[,"rowname"] <-  rep(colnames(loadings)[axes],nn)
+      if (xaxis_type == "numerical"){
+        melted[,"Var"] <-  round(as.numeric(rownames(loadings)),3)
+      } else{melted[,"Var"] <-  rownames(loadings)}
+    }
+    
+    # to silence CMD check
+    # in the future you will be able to use ggplot2::vars(.data$facet)
+    # but this is currently not supported (tidyverse/ggplot2#2963)
+    facet <- NULL; rm(facet)
+    
+    #######
+    
+    if (xaxis_type == "numerical") {
+      if (type.obj == "PCA") {
+        ylabs <- paste0("L", axes[i:last]," ",
+                        paste0("(", round(variance[i:last],2), "%)"))
+      }else {
+        ylabs <- paste0("L", i:last)
+      }
+      
+      plot_data <- data.frame(
+        facet = ylabs,
+        x = melted$Var,
+        y = melted$value
+      )
+      plot <- ggplot2::ggplot(data = plot_data, ggplot2::aes(x = .data$x, y = .data$y))
+      
+      # plot <- ggplot2::ggplot(data = melted, ggplot2::aes(x = Var, y = value))
+    } else  {
+      melted$index <- as.numeric(as.factor(melted$Var))
+      melted <- as.data.frame(melted)
+      
+      if (type.obj == "PCA") {
+        ylabs <- paste0("L", i:last," ",
+                        paste0("(", round(variance[i:last],2), "%)"))
+      }else {
+        ylabs <- paste0("L", i:last)
+      }
+      
+      plot_data <- data.frame(
+        facet = ylabs,
+        x = melted$index,
+        y = melted$value
+      )
+      
+      plot <- ggplot2::ggplot(data = plot_data, ggplot2::aes(x = .data$x, y = .data$y))
+      
+    }
+    
+    plot <- plot + ggplot2::theme_bw()
+    
+    if (loadingstype == "p") {
+      plot <- plot + ggplot2::geom_point(size=0.5)
+      if (xaxis_type == "character"){
+        plot <- plot + ggplot2::scale_x_continuous(breaks = seq(1, nn, floor(nn/nxaxis)),
+                                                   labels = rownames(loadings)[seq(1, nn, floor(nn/nxaxis))])
+      }
+    } else if (loadingstype == "l")  {
+      plot <- plot + ggplot2::geom_line()
+      if (xaxis_type == "character"){
+        plot <- plot + ggplot2::scale_x_continuous(breaks = seq(1, nn, floor(nn/nxaxis)),
+                                                   labels = rownames(loadings)[seq(1, nn, floor(nn/nxaxis))])
+      }
+    } else  {
+      
+      plot <- plot + ggplot2::geom_hline(yintercept = 0, size = 0.5, linetype = "dashed", colour = "gray60")
+      
+      if (xaxis_type == "numerical"){
+        plot <- plot + ggplot2::geom_segment(ggplot2::aes(xend = x, yend = 0),
+                                             size = 0.5, lineend = "round")
+      } else {
+        plot <- plot + ggplot2::geom_segment(ggplot2::aes(xend = x, yend = 0),
+                                             size = 0.5, lineend = "round")
+        
+        plot <- plot + ggplot2::scale_x_continuous(breaks = seq(1, nn, floor(nn/nxaxis)),
+                                                   labels = rownames(loadings)[seq(1, nn, floor(nn/nxaxis))])
+        
+      }
+      
+    }
+    
+    
+    plot <- plot + ggplot2::labs(title = main, x = xlab, y = ylab) +
+      ggplot2::facet_grid(facet ~  ., scales = "free_y") +
+      ggplot2::theme(axis.text.x = ggplot2::element_text(angle = as.numeric(ang), vjust = vjust, hjust = hjust,
+                                                         size = xaxis_size)) +
+      ggplot2::theme(strip.text.y = ggplot2::element_text(angle = 90)) +
+      ggplot2::theme(legend.position = "none")
+    if (!is.null(hline)){
+      plot <- plot + ggplot2::geom_hline(yintercept = hline, size = 0.5, linetype = "dashed", colour = "gray60")
+    }
+    
+    #
+    # #######
+    #     if (type.obj == "PCA") {
+    #       if (xaxis_type == "numerical")  {
+    #         plot <- plot + ggplot2::annotate("text", x = -Inf, y = Inf,
+    #                                          label = paste0("(",round(variance[i:last], 2), "%)"), vjust = 2, hjust = 1.5)
+    #       } else {
+    #         plot <- plot + ggplot2::annotate("text", x = 1, y = max(loadings),
+    #                                          label = paste0("(",round(variance[i:last], 2), "%)"), hjust = 0)
+    #       }
+    #
+    #       }
+    #
+    # #########
+    
+    
+    if (xaxis_type == "numerical")  {
+      if ((melted[1, "Var"] - melted[(dim(melted)[1]), "Var"]) > 0) {
+        plot <- plot + ggplot2::scale_x_reverse()
+      }
+    }
+    
+    plots[[j]] <- plot
+    i <- last + 1
+    
+    if (createWindow)  {
+      grDevices::dev.new(noRStudioGD = TRUE)
+    }
+    
+    # gridExtra::grid.arrange(plot)
+    j <- j + 1
+    
+  }
+  
+  return(plots)
+  
+}  # END
---
+++ R/DrawScores.R
@@ -0,0 +1,376 @@
+#' @export DrawScores
+#' @title Scores plots
+#'
+#' @description
+#' Draws scores plots for the SVDforPCA, PLSDA or OPLSDA functions.
+#'
+#' @param obj The objects resulting from a PCA, PLSDA or OPLSDA analysis.
+#' @param type.obj The type of object to be plotted.
+#' @param drawNames If \code{TRUE}, will show the observations names on the scores plot.
+#' @param createWindow If \code{TRUE}, will create a new window for the plot.
+#' @param main Plot title. If \code{NULL}, default title is provided.
+#' @param color Optional character, factor or numeric vector giving the color of the observations. If \code{length(color)} = 1, the unique color is kept for all the points.
+#' @param pch Optional character, factor or numeric vector giving the pch of the observations.
+#' @param size The points size.
+#' @param cex.lab The size of points labels.
+#' @param axes Numerical vector indicating the PC axes that are drawn. Only the two first values are considered for scores plot. See details#' @param num.stacked Number of stacked plots if \code{type} is \code{'loadings'}.
+#' @param xlab Label for the x-axis.
+#' @param ylab Label for the y-axis.
+#' @param drawEllipses If \code{TRUE}, will draw ellipses with the \code{ggplot2::stat_ellipse} with groups coresponding to the color vector.
+#' @param typeEl The type of ellipse, either "norm" (multivariate normal distribution), "t" (multivariate t-distribution) and "euclid" draws a circle with the radius equal to level, representing the euclidean distance from the center.
+#' @param levelEl The confidence level at which to draw an ellipse.
+#' @param drawPolygon If \code{TRUE}, will relate the points linked to the same group (color vector)
+#' @param noLegend If \code{TRUE}, no legend is drawn.
+#' @param legend_color_manual If not \code{NULL}, a named character vector giving manually the colors to be drawn, named with the levels of the color vector.
+#' @param legend_shape_manual If not \code{NULL}, a named character vector giving manually the shapes to be drawn, named with the levels of the pch vector.
+#' @return A score or loading plot in the current device.
+
+#' @details
+#' If \code{type.obj} is \code{'OPLSDA'}, axes = 1 represents the predictive score vector, axes = 2 represents the first orthogonal score vector, etc.
+#'
+#' @examples
+#'
+#' data('HumanSerum')
+#' res.PCA = SVDforPCA(HumanSerumSpectra)
+#' class = ClassHS
+#'
+#' DrawScores(res.PCA, drawNames=TRUE, type.obj = 'PCA',
+#' createWindow=FALSE, main = 'PCA score plot for HumanSerum dataset',
+#'   color = class, axes =c(1,2))
+#'
+#'
+#' @importFrom grDevices dev.new
+#' @import ggplot2
+#' @import reshape2
+#' @import gridExtra
+#' @importFrom plyr ddply
+
+DrawScores <- function(obj, type.obj = c("PCA", "PLSDA", "OPLSDA"), drawNames = TRUE,
+                       createWindow = FALSE, main = NULL, color = NULL, pch = NULL, size = 1,
+                       cex.lab = 3, axes = c(1, 2), xlab = NULL, ylab = NULL, drawEllipses = FALSE,
+                       typeEl = "norm", levelEl = 0.9, drawPolygon = FALSE, noLegend = FALSE,
+                       legend_color_manual = NULL, legend_shape_manual = NULL) {
+  
+  checkArg(main, "str", can.be.null = TRUE)
+  
+  # color = numeric, or factor
+  # pch = numeric, or factor
+  
+  
+  type.obj <- match.arg(type.obj)
+  
+  
+  m <- dim(obj$original.dataset)[1]
+  nn <- dim(obj$original.dataset)[2]
+  
+  
+  
+  # color
+  
+  if (!is.null(color) && is.vector(color, mode = "any") ) {
+    if (!length(color) %in% c(1,m)) {
+      stop("the length of color is not equal to 1 or the nrow of data matrix")
+    }else if (!is.null(legend_color_manual) & length(color)==1) {
+      legend_color_manual = NULL
+      warning("legend_color_manual is set to NULL since length(color)=1")
+    }else if (!is.null(legend_color_manual) & is.vector(legend_color_manual, mode = "any") &
+              length(legend_color_manual) != nlevels(as.factor(color))) {
+      stop("The length of legend_color_manual is not equal to the nlevels of color")
+    } else if (sum(!names(legend_color_manual) %in% levels(as.factor(color)))!=0) {
+      stop("The names of legend_color_manual are not equal to the levels names of color")
+    }
+  }
+  
+  # pch
+  if (!is.null(pch) && is.vector(pch, mode = "any")) {
+    if (length(pch) != m) {
+      stop("the length of pch is not equal to the nrow of data matrix")
+    } else if (!is.null(legend_shape_manual) & is.vector(legend_shape_manual, mode = "any") &
+               length(legend_shape_manual) != nlevels(as.factor(pch))) {
+      stop("The length of legend_shape_manual is not equal to the nlevels of pch")
+    } else if (sum(!names(legend_shape_manual) %in% levels(as.factor(pch)))!=0) {
+      stop("The names of legend_shape_manual are not equal to the levels names of pch")
+    }
+  }
+  
+  
+  
+  
+  
+  if(drawPolygon & is.null(color)) {
+    stop("drawPolygon is true but color is null ...")
+  }
+  
+  
+  
+  # axes
+  if (!is.vector(axes, mode = "numeric")) {
+    stop("axes is not a numeric vector")
+  }
+  
+  if(drawNames & is.null(rownames(obj$original.dataset))) {
+    warning("rownames of the dataset is NULL, hence you cannot use DrawNames=TRUE ==> reset to FALSE")
+    drawNames = FALSE
+  }
+  
+  
+  # Eigenvalues
+  if (type.obj == "PCA") {
+    eig <- obj$eigval
+    # Variances in percentage
+    variance <- eig * 100/sum(eig)
+  }
+  
+  
+  # scores
+  Xax <- axes[1]
+  Yax <- axes[2]
+  
+  if (type.obj == "PCA") {
+    XaxName <- paste0("PC", Xax, " (", round(variance[Xax], 2),"%)")
+    YaxName <- paste0("PC", Yax, " (", round(variance[Yax], 2), "%)")
+  } else if (type.obj == "OPLSDA") {
+    XaxName <- ifelse(Xax == 1, "Tp", paste0("To", Xax))
+    YaxName <- ifelse(Yax == 1, "Tp", paste0("To", Yax - 1))
+  } else { # PLS-DA
+    XaxName <- paste0("Tp", Xax)
+    YaxName <- paste0("Tp", Yax)
+  }
+  
+  
+  
+  if (type.obj == "OPLSDA") {
+    XaxName <- ifelse(Xax == 1, "Tp", paste0("To", Xax))
+    YaxName <- ifelse(Yax == 1, "Tp", paste0("To", Yax - 1))
+    
+    obj$scores <- cbind(Tp = obj$Tp, obj$Tortho)
+    colnames(obj$scores) <- c("Tp", paste0("To", 1:dim(obj$Tortho)[2]))
+  }
+  
+  class(obj$scores) <- "numeric"
+  scores <- as.data.frame(obj$scores)
+  
+  
+  # define color and pch
+  
+  if (!is.null(color)) {
+    color_factor <- as.factor(color)
+    namecolor <- deparse(substitute(color))
+  }
+  
+  if (!is.null(pch)) {
+    pch_factor <- as.factor(pch)
+    namepch <- deparse(substitute(pch))
+  }
+  
+  
+  plots <- list()
+  plot <- list()
+  Var <- rowname <- value <- NULL  # only for R CMD check
+  
+  ##########################################
+  
+  
+  # labs
+  if (is.null(xlab)) {
+    xlab <- XaxName
+  }
+  if (is.null(ylab)) {
+    ylab <- YaxName
+  }
+  
+  if (createWindow)  {
+    grDevices::dev.new(noRStudioGD = TRUE)
+  }
+  Xlim <- c(min(scores[, Xax]) * 1.4, max(scores[, Xax]) * 1.4)
+  Ylim <- c(min(scores[, Yax]) * 1.4, max(scores[, Yax]) * 1.4)
+  
+  
+  
+  if (!drawEllipses & !drawPolygon) {
+    plots <- ggplot2::ggplot(scores , ggplot2::aes(get(colnames(scores)[Xax]),
+                                                   get(colnames(scores)[Yax])))  + ggplot2::xlim(Xlim) + ggplot2::ylim(Ylim)
+    
+  }else {
+    plots <- ggplot2::ggplot(scores , ggplot2::aes(get(colnames(scores)[Xax]),get(colnames(scores)[Yax])))
+  }
+  
+  if (is.null(color) & is.null(pch)) {
+    # no color & no shape
+    plots <- plots + ggplot2::geom_point(size=size)
+    
+  } else if (!is.null(color) & is.null(pch)) {
+    # color
+    if (length(color)>1){
+      plots <- plots  +
+        ggplot2::geom_point(ggplot2::aes(colour = color_factor),size=size)
+    }else {
+      plots <- plots  +
+        ggplot2::geom_point(color=color,size=size)
+    }
+    
+    if (!is.null(legend_color_manual)) {
+      plots <- plots  +
+        ggplot2::scale_colour_manual(name = namecolor, breaks = color_factor,
+                                     values = legend_color_manual,
+                                     guide=guide_legend(order=1))+
+        guides(colour = guide_legend(override.aes = list(shape = 15)))
+    } else {
+      plots <- plots  +
+        scale_colour_discrete(name = namecolor, breaks = unique(color_factor),
+                              labels = as.character(unique(color)),
+                              guide=guide_legend(order=1))+
+        guides(colour = guide_legend(override.aes = list(shape = 15)))
+    }
+    
+    
+    if (drawPolygon) {
+      dataf <- cbind(color_factor, scores)
+      x <- colnames(scores)[Xax]
+      y <- colnames(scores)[Yax]
+      chulls <- plyr::ddply(dataf, .(color_factor), function(df) df[chull(df[,x], df[,y]), ])
+      
+      plots <- plots  +
+        geom_polygon(data = chulls, aes(get(colnames(scores)[Xax]),
+                                        get(colnames(scores)[Yax]), fill = color_factor, color = color_factor),
+                     alpha=0.4,show.legend = FALSE, size =0.3)
+      
+      
+    }
+    
+    if (drawEllipses) {
+      
+      plots <- plots + ggplot2::stat_ellipse(mapping = aes(get(colnames(scores)[Xax]),
+                                                           get(colnames(scores)[Yax]),
+                                                           colour = color_factor),
+                                             data = scores, type = typeEl,
+                                             level = levelEl)
+    }
+    
+  } else if (is.null(color) & !is.null(pch)) {
+    # shape
+    plots <- plots + ggplot2::geom_point(ggplot2::aes(shape = pch_factor), size=size)+
+      scale_shape_manual(name = namepch, values=seq(0,26),
+                         guide=guide_legend(order=1, shape = 1))
+    
+    
+    # legend_shape_manual
+    if (!is.null(legend_shape_manual)) {
+      plots <- plots  +
+        ggplot2::scale_shape_manual(name = namepch, breaks = pch_factor,
+                                    values = legend_shape_manual,
+                                    guide=guide_legend(order=1))
+    }
+    
+  } else {
+    # color + shape
+    if (namecolor!=namepch) {
+      
+      if (length(color)>1){
+        plots <- plots + ggplot2::geom_point(ggplot2::aes(colour = color_factor, shape = pch_factor), size=size)+
+          scale_shape_manual(values=seq(0,26), name = namepch)
+      }else {
+        plots <- plots + ggplot2::geom_point(ggplot2::aes(shape = pch_factor), color=color, size=size)+
+          scale_shape_manual(values=seq(0,26), name = namepch)
+      }
+      
+      
+    } else
+      plots <- plots + ggplot2::geom_point(ggplot2::aes(colour = color_factor, shape = color_factor), size=size)+
+        scale_shape_manual(name = namecolor, values=seq(0,26),
+                           guide=guide_legend(order=1, shape = 1))
+    
+    # legend_color_manual
+    if (!is.null(legend_color_manual)) {
+      plots <- plots  +
+        ggplot2::scale_colour_manual(name = namecolor, breaks = unique(color_factor),
+                                     values = legend_color_manual,
+                                     guide=guide_legend(order=1, shape = 1))+
+        guides(colour = guide_legend(override.aes = list(shape = 15)))
+      
+    }else {
+      plots <- plots  +
+        scale_colour_discrete(name = namecolor,
+                              guide=guide_legend(order=1, shape = 1))
+      if (namecolor!=namepch) {
+        plots <- plots  + guides(colour = guide_legend(override.aes = list(shape = 15)))
+      }
+    }
+    
+    # legend_shape_manual
+    if (namecolor!=namepch) {
+      if (!is.null(legend_shape_manual)) {
+        plots <- plots  +
+          ggplot2::scale_shape_manual(name = namepch, breaks = unique(pch_factor),
+                                      values = legend_shape_manual,
+                                      guide=guide_legend(order=1))
+      }
+      
+      
+    } else {
+      
+      if (!is.null(legend_shape_manual)) {
+        plots <- plots  +
+          ggplot2::scale_shape_manual(name = namecolor, breaks = unique(color_factor),
+                                      values = legend_shape_manual,
+                                      guide=guide_legend(order=1))
+      }
+      
+    }
+    
+    
+    if (drawPolygon) {
+      dataf <- cbind(color_factor, scores)
+      x <- colnames(scores)[Xax]
+      y <- colnames(scores)[Yax]
+      chulls <- plyr::ddply(dataf, .(color_factor), function(df) df[chull(df[,x], df[,y]), ])
+      
+      plots <- plots  +
+        geom_polygon(data = chulls, aes(get(colnames(scores)[Xax]),
+                                        get(colnames(scores)[Yax]), fill = color_factor, color = color_factor),
+                     alpha=0.4,show.legend = FALSE, size =0.3)
+    }
+    
+    
+    if (drawEllipses) {
+      
+      plots <- plots + ggplot2::stat_ellipse(mapping = aes(get(colnames(scores)[Xax]),
+                                                           get(colnames(scores)[Yax]),
+                                                           colour = color_factor),
+                                             data = scores, type = typeEl,
+                                             level = levelEl)
+    }
+    
+  }
+  
+  
+  
+  plots <- plots + ggplot2::labs(title = main, x = xlab, y = ylab) + ggplot2::geom_vline(xintercept = 0,
+                                                                                         size = 0.1) + ggplot2::geom_hline(yintercept = 0, size = 0.1) + ggplot2::theme_bw() +
+    ggplot2::theme(panel.grid.major = ggplot2::element_line(color = "gray60",
+                                                            size = 0.2), panel.grid.minor = ggplot2::element_blank(),
+                   panel.background = ggplot2::element_rect(fill = "gray98"))
+  
+  
+  if (drawNames) {
+    
+    if (is.null(color)) {
+      plots <- plots + ggplot2::geom_text(ggplot2::aes(x = scores[, Xax],
+                                                       y = scores[, Yax], label = rownames(obj$original.dataset)), hjust = 0,
+                                          nudge_x = (Xlim[2]/25), show.legend = FALSE, size = cex.lab)
+    } else {
+      plots <- plots + ggplot2::geom_text(ggplot2::aes(x = scores[, Xax],
+                                                       y = scores[, Yax], label = rownames(obj$original.dataset), colour = color_factor),
+                                          hjust = 0, nudge_x = (Xlim[2]/25), show.legend = F, size = cex.lab)
+    }
+  }
+  
+  if (noLegend){
+    plots <- plots + theme(legend.position="none")
+  }
+  
+  plots
+  
+  
+  
+}  # END
