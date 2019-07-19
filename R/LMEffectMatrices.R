abaa6ed5b7d716ba198287ce2831d02eb4033fe4
FranceschiniS <francseba@gmail.com>
2019-07-19 16:52:06 +0200

    Adding values documentation

--- R/LMEffectMatrices.R
+++ R/LMEffectMatrices.R
@@ -1,32 +1,32 @@
 #' @export LMEffectMatrices
 #' @title Computing the Effect Matrices
-#' @description Runs a GLM model and decomposes the outcomes into effect matrices
+#' @description Runs a GLM model and decomposes the outcomes into effect matrices for each model terms
 #'
 #' @param ResLMModelMatrix A list of 3 from \code{\link{LMModelMatrix}}
-#' @param outcomes A (n x y) matrix with n observations and y response variables
+#' @param outcomes A n x y matrix with n observations and y response variables
 #'
 #' @return A list with the following elements:
 #'  \describe{
-#'    \item{\code{formula}}{}
-#'    \item{\code{design}}{}
-#'    \item{\code{ModelMatrix}}{}
-#'    \item{\code{outcomes}}{}
-#'    \item{\code{effectMatrices}}{}
-#'    \item{\code{modelMatrixByEffect}}{}
-#'    \item{\code{predictedvalues}}{}
-#'    \item{\code{residuals}}{}
-#'    \item{\code{parameters}}{}
-#'    \item{\code{covariateEffectsNamesUnique}}{}
-#'    \item{\code{covariateEffectsNames}}{}
+#'    \item{\code{formula}}{A formula object with the expression of the GLM used to predict the outcomes}
+#'    \item{\code{design}}{A data frame with the "free encoded" experimental design}
+#'    \item{\code{ModelMatrix}}{A model matrix specifically encoded for the ASCA-GLM}
+#'    \item{\code{outcomes}}{A matrix with n observations and y response variables}
+#'    \item{\code{effectMatrices}}{A list of effect matrices for each model terms}
+#'    \item{\code{modelMatrixByEffect}}{A list of model matrix by models terms }
+#'    \item{\code{predictedvalues}}{A matrix with the predicted values}
+#'    \item{\code{residuals}}{A matrix with the residuals}
+#'    \item{\code{parameters}}{A matrix with the coefficients of every parameters by response variables}
+#'    \item{\code{covariateEffectsNamesUnique}}{A character vector with the name of the model terms}
+#'    \item{\code{covariateEffectsNames}}{A character vector with every name of the coefficients}
 #'  }
 #'
-#'  @examples
+#' @examples
 #'  data('UCH')
 #'  ResLMModelMatrix = LMModelMatrix(formula=UCH$formula,design=UCH$design)
-#'  LMEffectMatrices(ResLMModelMatrix,outcomes=outcomes)
+#'  LMEffectMatrices(ResLMModelMatrix,outcomes=UCH$outcomes)
 #'
-#'  @import stringr
-#'  @import plyr
+#' @import stringr
+#' @import plyr
 
 
 
@@ -34,8 +34,8 @@ LMEffectMatrices = function(ResLMModelMatrix,outcomes){
 
   #Checking the object
   if(!is.list(ResLMModelMatrix)){stop("Argument is not a list")}
-  if(length(ResLMModelMatrix)!=4){stop("List does not contain 4 arguments")}
-  if(names(ResLMModelMatrix)[1]!="formula"|names(ResLMModelMatrix)[2]!="design"|names(ResLMModelMatrix)[3]!="modelMatrix"){stop("Argument is not a ResLMModelMAtrix object")}
+  if(length(ResLMModelMatrix)!=3){stop("List does not contain 3 arguments")}
+  if(names(ResLMModelMatrix)[1]!="formula"|names(ResLMModelMatrix)[2]!="design"|names(ResLMModelMatrix)[3]!="ModelMatrix"){stop("Argument is not a ResLMModelMAtrix object")}
 
   #Attribute a name in the function environment
 
