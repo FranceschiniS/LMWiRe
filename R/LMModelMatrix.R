d142176445271db98f789a67e6f84757cd581f7e
FranceschiniS <francseba@gmail.com>
2019-08-01 16:24:24 +0200

    Modifications

--- R/LMModelMatrix.R
+++ R/LMModelMatrix.R
@@ -1,25 +1,35 @@
 #' @export LMModelMatrix
 #' @title Creating the model matrix
-#' @description Create the specific model matrix from the design and model matrices for the ASCA-GLM method
+#' @description Create the specific model matrix from the design for the ASCA-GLM method.
 #'
-#' @param formula The formula of the GLM used to predict the outcomes
-#' @param design The "free encoded" experimental design
+#' @param formula The \emph{p} terms formula of the GLM used to predict the response variables.
+#' @param design The nxk "free encoded" experimental design data frame. \strong{Variables must be factors}.
 #'
 #' @return A list with the 3 following named elements :
 #' \describe{
-#' \item{\code{formula}}{A \code{\link{formula}} object with the expression of the GLM used to predict the outcomes}
-#' \item{\code{design}}{A data frame with the "free encoded" experimental design}
-#' \item{\code{ModelMatrix}}{A model matrix specifically encoded for the ASCA-GLM}
+#' \item{\code{formula}}{A \code{\link{formula}} object with the expression of the GLM used to predict the outcomes.}
+#' \item{\code{design}}{A \emph{nxk} data frame with the "free encoded" experimental design.}
+#' \item{\code{ModelMatrix}}{A \emph{nxK} model matrix specifically encoded for the ASCA-GLM method.}
 #' }
+#'
+#' @details
+#'
+#' In typical ASCA-glm analysis, the GLM model need the design to be re-encoded with the commonly used in industrial experimental design \emph{sum coding}. The result is the model matrix.
+#' Suppose the design matrix is \emph{nxk} with n observations and \emph{k} factors. After the transformation the model matrix
+#' will be \emph{nxK}. For a parameter with a levels the re-encoding is \emph{a-1} colums with 0 and 1 for \emph{a-1} first levels and -1 for the last.
+#' \emph{K} is the sum of all the columns for every parameters.
 #'
 #' @seealso \code{\link{model.matrix}}
 #'
 #' More informations about the specific encoding is available in the article from (\emph{Thiel et al}, 2017)
 #'
 #' @examples
+#'
 #' data('UCH')
 #'
-#' ResLMModelMatrix = LMModelMatrix(as.formula(UCH$formula),UCH$design)
+#' ResLMModelMatrix <- LMModelMatrix(as.formula(UCH$formula),UCH$design)
+#'
+#' head(ResLMModelMatrix$ModelMatrix)
 #'
 #' @references Thiel M.,Feraud B. and Govaerts B. (2017) \emph{ASCA+ and APCA+: Extensions of ASCA and APCA
 #' in the analysis of unbalanced multifactorial designs}, Journal of Chemometrics
@@ -55,6 +65,16 @@ LMModelMatrix <- function(formula, design) {
   matchesVarNames <- varNames %in% names(design)
   if (!all(matchesVarNames, na.rm = FALSE)) {
     stop("Some of the variable names, present in the formula argument, do not correspond to one of the column names of the design argument. Please   adapt either one of both arguments.")
+  }
+
+  # Checking if all avariables are factors
+
+  if(all(names(Filter(is.factor, design))!=colnames(design))){
+    NoFactor = vector()
+    for(i in 1:length(colnames(design))){
+      NoFactor[i] = is.factor(design[,i])
+    }
+    stop(paste("Some of the variables from the design matrix are not factors :",colnames(design)[!NoFactor]))
   }
 
   # Checking which variables are factors
